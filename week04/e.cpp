4fb84ad186c374b7eb10f8da8c04b501c732854b
Julian Lueken <julian.lueken@stud.uni-goettingen.de>
2019-05-21 16:35:17 +0200

    big stink

--- week04/e.cpp
+++ week04/e.cpp
@@ -1,42 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int MAXN = 200, MAXM = 40000;
+int used[MAXN], match[MAXN], matchto[MAXM];
+vector<int> edges[MAXN];
 
-const int MAXN 	= 1000;
-const int SIDES = 6;
+bool dfs_kuhn(int v) {
+    used[v] = 1;
+    for(auto u: edges[v]) {
+        int mtu = matchto[u];
+        if(mtu == -1 || !used[mtu] && dfs_kuhn(mtu)) {
+            matchto[u] = v;
+            match[v] = u;
+            return true;
+        }
+    }
+    return false;
+}
+int kuhn(int n, int m) {
+    fill(match, match+n, -1);
+    fill(matchto, matchto+m, -1);
+    int cnt = 0;
+    for(int i = 0; i < n; i++) {
+        fill(used, used+n, 0);
+        if(dfs_kuhn(i))
+            cnt++;
+    }
+    return cnt;
+}
 
-char cubes[MAXN][SIDES];
-vector<int> index[MAXN];
+set<char> cube[MAXN];
+void add_cube(int u) {
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.length(); i++)
+		cube[u].insert(s[i]);
+}
+
+void add_edge(int u, int v) {
+	edges[u].push_back(v);
+}
 
 int main() {
-	int N;
-	cin >> N;
-
-	char c[SIDES];
-	for (int i = 0; i < N; i++) {
-		cin >> c;
-		for (int j = 0; j < SIDES; j++) {
-			cubes[i][j] = c[j];	
-		}
-	}
-
-	char name[MAXN];
+    int M;
+	cin >> M;
+    
+    string name;
 	cin >> name;
-	int k = 0;
-	while (name[k] != '\0') {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < SIDES; j++) {
-				if (name[k] == cubes[i][j]) {
-					index[k].push_back(i);
-				}
-			}
-		}
-		k++;
+
+	for (int i = 0; i < M; i++) {
+		add_cube(i);
+		for (int j = 0; j < name.length(); j++)
+			if (cube[i].find(name[j]) != cube[i].end())
+				add_edge(j,i);
 	}
 
-	k = 0;
-	while (name[k] != '\0') {
-		if ( find(index[k].begin(), index[k].end(), k) ) 
-	}
-
-
+	int K = kuhn(name.length(), M);
+	
+	if (K >= name.length())		cout << "YES";
+	else						cout << "NO";
+	cout << "\n";
 }
